11121e742296aa817687e8fda470d7116705667e
Alexis <55393916+Tsunaa-Alexis@users.noreply.github.com>
2020-05-27 13:53:13 +0200

    Update modifmetauniq.php

--- modifmetauniq/modifmetauniq.php
+++ modifmetauniq/modifmetauniq.php
@@ -91,10 +91,10 @@ ini_set('error_reporting', E_ALL);
 $host = 'localhost';
 $db   = 'testest';
 $user = 'root';
-$pass = 'root';
+$pass = '';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=3308;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
